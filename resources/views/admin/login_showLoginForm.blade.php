efe3b3f647961b9eb3e50c88a489db29359d57ba
alan <crwu0206@gmail.com>
2021-10-21 02:49:25 +0800

    fix login page

--- resources/views/admin/login_showLoginForm.blade.php
+++ resources/views/admin/login_showLoginForm.blade.php
@@ -5,70 +5,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('global.admin.app_title') }} - 登入</title>
-    <link rel="stylesheet" href="{{ asset('/vendor/laravel-layui-admin/lib/layui/css/layui.css') }}">
-    <link rel="stylesheet" href="{{ asset('/vendor/laravel-layui-admin/css/admin.css') }}">
+    <link rel="stylesheet" href="{{ ADMIN_CSS }}/login.css" />
+    <link rel="stylesheet" href="{{ PLUGINS }}/laravel-layui-admin/lib/layui/css/layui.css">
 </head>
-<body class="layui-layout-body" style="background: #4e97d9;">
-<canvas class="background" ></canvas>
-<div id="admin-login">
-    <form class="layui-form" lay-filter="form-box" action="" method="POST">
-        @include('admin.common.msg')
-        {{ csrf_field() }}
-        <h2>{{ config('global.admin.app_title') }}</h2>
-        <div class="layui-form-item">
-            <label class="layui-form-label">用戶名</label>
-            <div class="layui-input-block">
-                <input type="text" name="username" required  lay-verify="required" lay-verType="" placeholder="請輸入用戶名" autocomplete="off" class="layui-input">
-            </div>
+<body>
+<div id="logincontainer">
+    <div id="loginbox">
+        <div id="loginheader">
+            <img src="{{ ADMIN_IMG }}/hanboxLogoA.png" alt="Control Panel Login" />
+            {{--            <h2>{{ config('global.admin.app_title') }}</h2>--}}
         </div>
-        <div class="layui-form-item">
-            <label class="layui-form-label">密碼</label>
-            <div class="layui-input-block">
-                <input type="password" name="password" required lay-verify="required" lay-verType=""
-                       placeholder="請輸入密碼" autocomplete="off" class="layui-input">
-            </div>
+        <div id="innerlogin">
+            <form class="layui-form" lay-filter="form-box" action="" method="POST">
+                {{ csrf_field() }}
+                @include('admin.common.msg')
+                <div class="layui-form-item">
+                    <label class="layui-form-label layui-form-mid" style="color: #FAFAFA">用戶名</label>
+                    <div class="layui-input-block">
+                        <input type="text" name="username" required  lay-verify="required" lay-verType=""
+                               placeholder="請輸入用戶名" autocomplete="off" class="layui-input">
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <label class="layui-form-label layui-form-mid" style="color: #FAFAFA">密　碼</label>
+                    <div class="layui-input-block">
+                        <input type="password" name="password" required lay-verify="required" lay-verType=""
+                               placeholder="請輸入密碼" autocomplete="off" class="layui-input">
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <button type="submit" class="layui-btn" lay-submit lay-filter="login">立即登入</button>
+                    <button type="button" class="layui-btn layui-btn-primary">忘記密碼</button>
+                </div>
+            </form>
         </div>
-{{--        <div class="layui-form-item">--}}
-{{--            <label class="layui-form-label">驗證碼</label>--}}
-{{--            <div class="layui-input-block">--}}
-{{--                <input maxlength="4" size="8" name="captcha" id="captcha" type="text"--}}
-{{--                       required  lay-verify="required" placeholder="請輸入驗證碼" autocomplete="off" class="layui-input" />--}}
-{{--                <img src="{{ captcha_src('flat') }}" style="cursor: pointer" onclick="this.src='{{ captcha_src('flat') }}'+Math.random()">--}}
-{{--            </div>--}}
-{{--        </div>--}}
-        <div class="layui-form-item">
-            <div class="layui-input-block">
-                <button type="submit" class="layui-btn" lay-submit lay-filter="login">立即登入</button>
-                <button type="button" class="layui-btn layui-btn-primary">忘記密碼</button>
-            </div>
-        </div>
-    </form>
+    </div>
+    <img src="{{ ADMIN_IMG }}/login_fade.png" alt="Fade" />
 </div>
-<script src="{{ asset('/vendor/laravel-layui-admin/lib/layui/layui.js') }}"></script>
-<script src="{{ asset('/vendor/laravel-layui-admin/js/admin.js') }}"></script>
-<script src="{{ asset('/vendor/laravel-layui-admin/js/particles.min.js') }}"></script>
+<script src="{{ PLUGINS }}/laravel-layui-admin/lib/layui/layui.js"></script>
+<script src="{{ PLUGINS }}/laravel-layui-admin/js/admin.js"></script>
 <script>
-    window.onload = function() {
-        Particles.init({
-            selector: '.background',
-            color: '#ffffff',
-            maxParticles: 80,
-            connectParticles: true,
-            responsive: [
-                {
-                    breakpoint: 768,
-                    options: {
-                        maxParticles: 80
-                    }
-                }, {
-                    breakpoint: 375,
-                    options: {
-                        maxParticles: 50
-                    }
-                }
-            ]
-        });
-    };
     //JavaScript代碼区域
     var verify = {
     };
@@ -79,25 +55,7 @@
 
         form.verify(verify);
         form.on('submit(login)',function(data) {
-            //console.log(data.field);
-            //return false; //阻止表單跳转
-            // 用于提交成功初始化表單
-            // form.val("form-box",{
-            //     'username':''
-            // });
         });
-
-        {{--form.on('submit(login)', function(data){--}}
-        {{--    $.post("{{ route("admin.login.login") }}", data.field, function(response) {--}}
-        {{--        if (response.code === 0) {--}}
-        {{--            layui.layer.msg(response.msg, {time: 2000, icon: 6});--}}
-        {{--            window.location = "{{ route("admin.index.index") }}";--}}
-        {{--        } else {--}}
-        {{--            layui.layer.msg(response.msg, {time: 3000, icon: 5});--}}
-        {{--        }--}}
-        {{--    });--}}
-        {{--    return false;--}}
-        {{--});--}}
     });
 </script>
 </body>
--- resources/views/web/login_showLoginForm.blade.php
+++ resources/views/web/login_showLoginForm.blade.php
@@ -4,73 +4,48 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('global.web.app_title') }} - 會員登入</title>
-    <link rel="stylesheet" href="{{ asset('/vendor/laravel-layui-admin/lib/layui/css/layui.css') }}">
-    <link rel="stylesheet" href="{{ asset('/vendor/laravel-layui-admin/css/admin.css') }}">
+    <title>{{ config('global.web.app_title') }} - 登入</title>
+    <link rel="stylesheet" href="{{ WEB_CSS }}/sienergy/login.css" />
+    <link rel="stylesheet" href="{{ PLUGINS }}/laravel-layui-admin/lib/layui/css/layui.css">
 </head>
-<body class="layui-layout-body" style="background: #4e97d9;">
-<canvas class="background" ></canvas>
-<div id="admin-login">
-    <form class="layui-form" lay-filter="form-box" action="" method="POST">
-        @include('web.common.msg')
-        {{ csrf_field() }}
-        <h2>{{ config('global.web.app_title') }}</h2>
-        <div class="layui-form-item">
-            <label class="layui-form-label">用戶名</label>
-            <div class="layui-input-block">
-                <input type="text" name="username" required  lay-verify="required" lay-verType=""
-                       placeholder="請輸入用戶名" autocomplete="off" class="layui-input">
-            </div>
+<body>
+<div id="logincontainer">
+    <div id="loginbox">
+        <div id="loginheader">
+            <img src="{{ WEB_IMG }}/hanboxLogoA.png" alt="Control Panel Login" />
+            {{--            <h2>{{ config('global.web.app_title') }}</h2>--}}
         </div>
-        <div class="layui-form-item">
-            <label class="layui-form-label">密碼</label>
-            <div class="layui-input-block">
-                <input type="password" name="password" required lay-verify="required" lay-verType=""
-                       placeholder="請輸入密碼" autocomplete="off" class="layui-input">
-            </div>
+        <div id="innerlogin">
+            <form class="layui-form" lay-filter="form-box" action="" method="POST">
+                {{ csrf_field() }}
+                @include('web.common.msg')
+                <div class="layui-form-item">
+                    <label class="layui-form-label layui-form-mid" style="color: #FAFAFA">用戶名</label>
+                    <div class="layui-input-block">
+                        <input type="text" name="username" required  lay-verify="required" lay-verType=""
+                               placeholder="請輸入用戶名" autocomplete="off" class="layui-input">
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <label class="layui-form-label layui-form-mid" style="color: #FAFAFA">密　碼</label>
+                    <div class="layui-input-block">
+                        <input type="password" name="password" required lay-verify="required" lay-verType=""
+                               placeholder="請輸入密碼" autocomplete="off" class="layui-input">
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <button type="submit" class="layui-btn" lay-submit lay-filter="login">立即登入</button>
+                    <button type="button" class="layui-btn layui-btn-primary">忘記密碼</button>
+                </div>
+            </form>
         </div>
-{{--        <div class="layui-form-item">--}}
-{{--            <label class="layui-form-label">驗證碼</label>--}}
-{{--            <div class="layui-input-block">--}}
-{{--                <input maxlength="4" size="8" name="captcha" id="captcha" type="text"--}}
-{{--                       required  lay-verify="required" placeholder="請輸入驗證碼" autocomplete="off" class="layui-input" />--}}
-{{--                <img src="{{ captcha_src('flat') }}" style="cursor: pointer" onclick="this.src='{{ captcha_src('flat') }}'+Math.random()">--}}
-{{--            </div>--}}
-{{--        </div>--}}
-        <div class="layui-form-item">
-            <div class="layui-input-block">
-                <button type="submit" class="layui-btn" lay-submit lay-filter="login">立即登入</button>
-                <button type="button" class="layui-btn layui-btn-primary">忘記密碼</button>
-            </div>
-        </div>
-    </form>
+    </div>
+    <img src="{{ WEB_IMG }}/login_fade.png" alt="Fade" />
 </div>
-<script src="{{ asset('/vendor/laravel-layui-admin/lib/layui/layui.js') }}"></script>
-<script src="{{ asset('/vendor/laravel-layui-admin/js/admin.js') }}"></script>
-<script src="{{ asset('/vendor/laravel-layui-admin/js/particles.min.js') }}"></script>
+<script src="{{ PLUGINS }}/laravel-layui-admin/lib/layui/layui.js"></script>
+<script src="{{ PLUGINS }}/laravel-layui-admin/js/admin.js"></script>
 <script>
-    window.onload = function() {
-        Particles.init({
-            selector: '.background',
-            color: '#ffffff',
-            maxParticles: 80,
-            connectParticles: true,
-            responsive: [
-                {
-                    breakpoint: 768,
-                    options: {
-                        maxParticles: 80
-                    }
-                }, {
-                    breakpoint: 375,
-                    options: {
-                        maxParticles: 50
-                    }
-                }
-            ]
-        });
-    };
-    //JavaScript代码区域
+    //JavaScript代碼区域
     var verify = {
     };
 
@@ -80,12 +55,6 @@
 
         form.verify(verify);
         form.on('submit(login)',function(data) {
-            //console.log(data.field);
-            //return false; //阻止表单跳转
-            // 用于提交成功初始化表单
-            // form.val("form-box",{
-            //     'username':''
-            // });
         });
     });
 </script>
